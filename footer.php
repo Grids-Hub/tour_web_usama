cf7196c2f46ff2262b67fadcf815da7dc7f81ccf
usamagrid7016 <osama.gridshub@gmail.com>
2022-03-24 15:36:31 +0500

    putted all scripts in functions.php

--- footer.php
+++ footer.php
@@ -103,17 +103,8 @@
       </div>
 
     </footer>
-
-    <!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
-<script src="./wp-content/themes/usama-wordpress-theme/build/js/all.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
-
-<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="/resources/demos/style.css">
+ <!-- Bootstrap core JavaScript -->
+   <?php wp_footer(); ?>
     
   </body>
 </html>
--- header.php
+++ header.php
@@ -13,6 +13,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
      crossorigin="anonymous">
+     
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
 
      <!-- Custom fonts for this template -->
      <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -29,7 +32,8 @@
 		html { margin-top: 0px !important; }
 	}
 </style>
-  
+
+   
   
 </head>
 <body>
